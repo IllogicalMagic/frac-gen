c39705eb5fde51d8af0a27f5a7135093ed63bcba
IllogicalMagic <coillol@yandex.ru>
2018-10-21 23:27:03 +0300

    More type refactoring

--- Config.h
+++ Config.h
@@ -1,11 +1,13 @@
 #ifndef FRACTAL_CONFIG_H
 #define FRACTAL_CONFIG_H
 
+#include "Types.h"
+
 constexpr int OffsetX = 0;
 constexpr int OffsetY = 0;
 
-constexpr double CX = 0;
-constexpr double CY = 0;
+constexpr FloatType CX = 0;
+constexpr FloatType CY = 0;
 
 constexpr int MinX = -500 + OffsetX;
 constexpr int MaxX = 500 + OffsetX;
@@ -14,12 +16,8 @@ constexpr int MinY = -500 + OffsetY;
 constexpr int MaxY = 500 + OffsetY;
 constexpr int YLen = MaxY - MinY;
 
-constexpr double Scale = 20.0;
+constexpr FloatType Scale = 20.0;
 
 constexpr int MaxIters = 100;
-
-constexpr int CheckOnEveryNIters = 0x0;
-
-constexpr int GlobalDefaultPrec = 10;
 
 #endif
--- Types.h
+++ Types.h
@@ -2,15 +2,10 @@
 #define FRACTAL_TYPES_H
 
 #include <complex>
-#include <sstream>
-#include <type_traits>
-#include <vector>
+#include <utility>
 
 using FloatType = double;
 using ValType = std::complex<FloatType>;
-// Coefficients of polynom starting from lowest power.
-// e.g. poly[0] is a constant (c*x^0).
-using PolyType = std::vector<ValType>;
 
 /* using PtColor = std::pair<int, unsigned>; */
 using PtColor = std::pair<bool, double>;
