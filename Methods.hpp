3697fa537f01480e9b8282304a553eeef66cbea1
IllogicalMagic <coillol@yandex.ru>
2018-10-31 02:39:31 +0300

    Add circular buffer for points

--- Methods.hpp
+++ Methods.hpp
@@ -2,6 +2,7 @@
 #define FRACGEN_ITER_METHODS_DEFINED__
 
 #include "Config.h"
+#include "Support.hpp"
 #include "TypeHelpers.hpp"
 #include "Types.h"
 
@@ -18,42 +19,37 @@ static ValType calcNextSteffensonL(CopyOrRefT<FnTy> Fn, ValType Pt) {
   return Pt - (Tmp * Tmp) / (Tmp2 - Tmp);
 }
 
-template<typename T, T Num, typename = std::enable_if_t<std::is_integral_v<T>, void>>
-constexpr T nextPow2() {
-  static_assert(!(Num & (1 << (sizeof(Num) * 8 - 1))), "Too big number");
-  if (Num == 0)
-    return 1;
-  T Res = 1;
-  while ((Res <<= 1) < Num);
-  return Res;
-}
-
-template<IdxType Mask>
-struct IdxGetT {
-private:
-  IdxType Cur = 0;
-public:
-  IdxGetT() = default;
-  IdxType operator()(IdxType Idx) { return (Cur + Idx) & Mask; }
-  void operator++() { Cur = (Cur + 1) & Mask; }
-};
-
 template<typename FnT, typename NormT, IdxType Degree = 7>
 struct CalcNextSidi {
   static constexpr IdxType UsedPts = Degree + 1;
-  static constexpr IdxType StorageSize = nextPow2<IdxType, UsedPts>();
 
   using FnTy = CopyOrRefT<FnT>;
   using NormTy = CopyOrRefT<NormT>;
 
 private:
   static constexpr IdxType PNum = UsedPts;
+  static constexpr IdxType StorageSize = nextPow2<IdxType, PNum>();
+  static constexpr IdxType Mask = StorageSize - 1;
+  static_assert((Mask & (Mask + 1)) == 0, "Mask should be power of 2 minus one!");
+
   // Differences window.
+  // Should it be separated into another class???
   ValType DW[PNum][StorageSize];
+  IdxType DWPos = 0;
   ValType Diffs[Degree];
 
+  IdxType getDWIdx(IdxType Idx) {
+    // TODO: fix indices and add an assertion.
+    return (DWPos + Idx) & Mask;
+  }
+
+  void advanceDWIdx() {
+    DWPos = getDWIdx(1);
+  }
+
 public:
-  CalcNextSidi(FnTy Fn, const ValType Pts[]) {
+  template<typename PtCont>
+  CalcNextSidi(FnTy Fn, const PtCont &Pts) {
     for (IdxType i = 0; i < PNum; ++i)
       DW[0][i] = Fn(Pts[i]);
 
@@ -66,30 +62,31 @@ public:
       Diffs[i] = Pts[Degree] - Pts[i];
   }
 
-  template<typename IdxGetTy>
-  ValType get(FnTy Fn, NormTy Norm,
-              const ValType Pts[], IdxGetTy IdxGet) {
-    IdxType Cur = IdxGet(Degree);
+  template<typename PtCont>
+  ValType get(FnTy Fn, NormTy Norm, const PtCont &Pts) {
+    IdxType Cur = getDWIdx(Degree);
 
     ValType Last = DW[Degree][Cur];
     ValType Drv = Last;
     for (IdxType j = Degree - 1; j > 0; --j) {
       Drv *= Diffs[j - 1];
-      Last = DW[j][IdxGet(j)] + Last * Pts[j]; // Probably an error. TODO: check.
+      Last = DW[j][getDWIdx(j)] + Last * Pts[j]; // Definitely an error. TODO: fix.
       Drv += Last;
     }
 
-    return Pts[Cur] + DW[0][Cur] / Drv;
+    return Pts[Degree] + DW[0][Cur] / Drv;
   }
 
-  template<typename IdxGetTy>
-  void update(FnTy Fn, const ValType Pts[], IdxGetTy IdxGet) {
+  template<typename PtCont>
+  void update(FnTy Fn, const PtCont &Pts) {
+    advanceDWIdx();
     // Renew table.
-    DW[0][IdxGet(Degree)] = Fn(Pts[IdxGet(Degree)]);
+    // TODO: check all indices.
+    DW[0][getDWIdx(Degree)] = Fn(Pts[Degree]);
     for (IdxType j = 1; j < PNum; ++j) {
-      DW[j][IdxGet(Degree + j)] =
-        (DW[j - 1][IdxGet(Degree + j)] - DW[j - 1][IdxGet(Degree + j - 1)]) /
-        (Pts[IdxGet(Degree)] - Pts[IdxGet(Degree - j)]);
+      DW[j][getDWIdx(Degree + j)] =
+        (DW[j - 1][getDWIdx(Degree + j)] - DW[j - 1][getDWIdx(Degree + j - 1)]) /
+        (Pts[Degree] - Pts[Degree - j]);
     }
   }
 };
@@ -97,19 +94,18 @@ public:
 template<typename FnT, typename NormT>
 struct CalcNextMuller {
   static constexpr IdxType UsedPts = 3;
-  static constexpr IdxType StorageSize = nextPow2<IdxType, UsedPts>();
 
   using FnTy = CopyOrRefT<FnT>;
   using NormTy = CopyOrRefT<NormT>;
 
-  CalcNextMuller(FnTy Fn, const ValType Pts[]) {}
+  template<typename PtCont>
+  CalcNextMuller(FnTy Fn, const PtCont &Pts) {}
 
-  template<typename IdxGetTy>
-  ValType get(FnTy Fn, NormTy Norm,
-              const ValType Pts[], IdxGetTy IdxGet) {
-    ValType P0 = Pts[IdxGet(0)];
-    ValType P1 = Pts[IdxGet(1)];
-    ValType P2 = Pts[IdxGet(2)];
+  template<typename PtCont>
+  ValType get(FnTy Fn, NormTy Norm, const PtCont &Pts) {
+    ValType P0 = Pts[0];
+    ValType P1 = Pts[1];
+    ValType P2 = Pts[2];
 
     ValType F0 = Fn(P0);
     ValType F1 = Fn(P1);
@@ -126,8 +122,8 @@ struct CalcNextMuller {
     return P2 - 2.0 * F2 / Den;
   }
 
-  template<typename IdxGetTy>
-  void update(FnTy Fn, const ValType Pts[], IdxGetTy IdxGet) {}
+  template<typename PtCont>
+  void update(FnTy Fn, const PtCont &Pts) {}
 };
 
 template<typename Method, typename ColorFnTy>
@@ -137,28 +133,26 @@ getPointIndexN(typename Method::FnTy Fn,
                CopyOrRefT<ColorFnTy> ColorFn,
                ValType Init) {
   constexpr IdxType UsedPts = Method::UsedPts;
-  constexpr IdxType Size = Method::StorageSize;
 
-  ValType Pts[Size];
-  Pts[0] = Init;
-  for (unsigned i = 1; i < UsedPts; ++i)
-    Pts[i] = calcNextSteffensonL<decltype(Fn)>(Fn, Pts[i - 1]);
+  CircularBuffer<ValType, UsedPts> Pts([Fn, Pt = Init]() mutable -> ValType {
+      ValType Old = Pt;
+      Pt = calcNextSteffensonL<decltype(Fn)>(Fn, Pt);
+      return Old;
+    });
 
   Method Mth(Fn, Pts);
 
-  IdxGetT<Size - 1> IdxGet;
   for (int i = 0; i < MaxIters; ++i) {
-    ValType Next = Mth.get(Fn, Norm, Pts, IdxGet);
+    ValType Next = Mth.get(Fn, Norm, Pts);
     if (std::isnan(Next.real()) || std::isnan(Next.imag()))
       break;
 
     if (Norm(Fn(Next)) < Epsilon)
       return {true, ColorFn(Next)};
 
-    ++IdxGet;
-    Pts[IdxGet(UsedPts)] = Next;
+    Pts.push_back(Next);
 
-    Mth.update(Fn, Pts, IdxGet);
+    Mth.update(Fn, Pts);
   }
 
   return {false, 0.0};
---
+++ Support.hpp
@@ -0,0 +1,63 @@
+#ifndef FRACGEN_SUPPORT_HPP_DEFINED__
+#define FRACGEN_SUPPORT_HPP_DEFINED__
+
+#include "Types.h"
+
+#include <algorithm>
+#include <array>
+
+#include <cassert>
+
+template<typename T, T Num, typename = std::enable_if_t<std::is_integral_v<T>, void>>
+constexpr T nextPow2() {
+  static_assert(!(Num & (1 << (sizeof(Num) * 8 - 1))), "Too big number");
+  if (Num == 0)
+    return 1;
+  T Res = 1;
+  while ((Res <<= 1) < Num);
+  return Res;
+}
+
+// Specialized always full circular buffer.
+template<typename T, IdxType Size>
+class CircularBuffer {
+  static constexpr IdxType StorageSize = nextPow2<IdxType, Size>();
+  static constexpr IdxType Mask = StorageSize - 1;
+  static_assert((Mask & (Mask + 1)) == 0, "Mask should be power of 2 minus one!");
+
+  std::array<T, StorageSize> Data;
+  IdxType Begin = 0;
+
+  IdxType getIdx(IdxType Idx) const {
+    assert(Idx < Size && "Idx is out of range");
+    return (Begin + Idx) & Mask;
+  }
+public:
+  // Sorry, no default ctor.
+  CircularBuffer() = delete;
+
+  template<typename GenTy>
+  CircularBuffer(GenTy Generator) {
+    std::generate(Data.begin(), Data.begin() + Size, Generator);
+  }
+
+  const T &operator[](IdxType Idx) const {
+    return Data[getIdx(Idx)];
+  }
+
+  T &operator[](IdxType Idx) {
+    return Data[getIdx(Idx)];
+  }
+
+  void push_back(const T &Val) {
+    Begin = getIdx(1);
+    (*this)[Size - 1] = Val;
+  }
+
+  void push_back(T &&Val) {
+    Begin = getIdx(1);
+    (*this)[Size - 1] = std::move(Val);
+  }
+};
+
+#endif
