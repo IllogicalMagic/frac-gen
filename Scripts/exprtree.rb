8cd91175920518cb20c7465f135e539e6c70663f
IllogicalMagic <coillol@yandex.ru>
2019-02-03 20:34:56 +0300

    Separate and simplify expression generation

---
+++ Scripts/exprtree.rb
@@ -0,0 +1,369 @@
+module ExprTree
+
+  def self.set_tern_mode(val)
+    $generate_ternary = val
+  end
+
+  def self.init_rng(seed)
+    $rng = Random.new(seed)
+  end
+
+  class BadExpr < StandardError
+  end
+
+  Point = 'Pt'
+  Num = 'num'
+
+  class Node
+    attr_reader :operands
+    attr_reader :fn
+
+    def generate_operand(type, level)
+      case type
+      when :point
+        Leaf.new(Point)
+      when :num
+        Leaf.new(Num)
+      when :tern
+        Ternary.new(level + 1)
+      else
+        sel = ExprTree.send(type)
+        ops = sel[$rng.rand(sel.size)]
+        Func.new(*ops, level + 1)
+      end
+    end
+
+    def initialize(level)
+      if level == 50
+        fail BadExpr
+      end
+    end
+
+    def to_s
+      fail "Impossible"
+    end
+  end # class Node
+
+  class Func < Node
+    PROB_RANGE = 15
+    INIT_TERN = 2
+    INIT_FUNC = 12
+
+    LEAF_RANGE = 10
+    LEAF_NUM = 3
+
+    MAX_ARITY = 4
+
+    attr_reader :fn
+    attr_reader :optype
+    attr_accessor :operands
+
+    def initialize(fn, arity, optype, level, operands = nil)
+      super(level)
+      @fn = fn
+      if operands.nil?
+        @operands = []
+        arity ||= $rng.rand(2..MAX_ARITY)
+        arity.times do
+          # Randomize node only if it is a simple function.
+          # Otherwise there could be a problem with logical nodes.
+          if optype == :simple_fn
+            num = $rng.rand(PROB_RANGE + level)
+            if num < INIT_TERN && $generate_ternary
+              optype = :tern
+            elsif num >= INIT_FUNC
+              num = $rng.rand(LEAF_RANGE)
+              optype = num < LEAF_NUM ? :num : :point
+            end
+          end
+          @operands << generate_operand(optype, level)
+        end
+      else
+        @operands = operands
+      end
+    end
+
+    def get_inv(op, neg = false, value = nil)
+      if value
+        val = value
+      else
+        val = neg ? "-1.0" : "1.0"
+      end
+      Func.new("/", 2, nil, 0, [Leaf.new(val), op])
+    end
+
+    def get_sqr(op)
+      Func.new("*", 2, nil, 0, [op, op])
+    end
+
+    def get_prd(op1, op2)
+      Func.new("*", 2, nil, 0, [op1, op2])
+    end
+
+    def get_x_m_one(op, inv = false)
+      ops = [op, Leaf.new("1.0")]
+      ops = ops.reverse if inv
+      Func.new("-", 2, nil, 0, ops)
+    end
+
+    def get_x_p_one(op, inv = false)
+      Func.new("+", 2, nil, 0, [op, Leaf.new("1.0")])
+    end
+
+    def get_sqrt(op)
+      Func.new("sqrt", 1, nil, 0, [op])
+    end
+
+    def diff
+      func = @fn
+      case func
+      when "+", "-"
+        new_ops = @operands.map{ |o| o.diff }
+        res = Func.new(func, new_ops.size, nil, 0, new_ops)
+      when "*"
+        new_ops = Array.new
+        @operands.each_with_index do |o, i|
+          ops = @operands[0...i] + @operands[(i + 1)..-1]
+          mult = Func.new("*", @operands.size, nil, 0, [o.diff] + ops)
+          new_ops << mult
+        end
+        res = Func.new("+", new_ops.size, nil, 0, new_ops)
+      when "/"
+        # Like * but should invert all but first operands.
+        if @operands.size != 2
+          invops = Array.new
+          invops = @operands[1..-1].map do |op|
+            get_inv(op)
+          end
+          invops << @operands[0]
+          inverted = Func.new("*", @operands.size, nil, 0, invops)
+          res = inverted.diff
+        else
+          op1 = @operands[0]
+          op2 = @operands[1]
+          sqr = get_sqr(op2)
+          op1d = op1.diff
+          op2d = op2.diff
+          lhs = get_prd(op1d, op2)
+          rhs = get_prd(op1, op2d)
+          diff = Func.new("-", 2, nil, 0, [lhs, rhs])
+          res = Func.new("/", 2, nil, 0, [diff, sqr])
+        end
+      when "sin"
+        op = @operands.first
+        drv = Func.new("cos", 1, nil, 0, [op])
+        res = get_prd(drv, op.diff)
+      when "cos"
+        op = @operands.first
+        dcos = Func.new("sin", 1, nil, 0, [op])
+        drv = Func.new("*", 2, nil, 0, [dcos, Leaf.new("-1.0")])
+        res = get_prd(drv, op.diff)
+      when "tan"
+        op = @operands.first
+        cos = Func.new("cos", 1, nil, 0, [op])
+        sqr = get_sqr(cos)
+        drv = get_inv(sqr)
+        res = get_prd(drv, op.diff)
+      when "asin", "acos"
+        op = @operands.first
+        sqr = get_sqr(op)
+        diff = get_x_m_one(sqr, true)
+        sqrt = get_sqrt(diff)
+        drv = get_inv(sqrt, func == "acos")
+        res = get_prd(drv, op.diff)
+      when "atan"
+        op = @operands.first
+        sqr = get_sqr(op)
+        sum = get_x_p_one(sqr)
+        drv = get_inv(sum)
+        res = get_prd(drv, op.diff)
+      when "sinh"
+        op = @operands.first
+        drv = Func.new("cosh", 1, nil, 0, [op])
+        res = get_prd(drv, op.diff)
+      when "cosh"
+        op = @operands.first
+        drv = Func.new("sinh", 1, nil, 0, [op])
+        res = get_prd(drv, op.diff)
+      when "tanh"
+        op = @operands.first
+        cosh = Func.new("cosh", 1, nil, 0, [op])
+        sqr = get_sqr(cosh)
+        drv = get_inv(sqr)
+        res = get_prd(drv, op.diff)
+      when "asinh", "acosh"
+        op = @operands.first
+        sqr = get_sqr(op)
+        poly = func == "asinh" ? get_x_p_one(sqr) : get_x_m_one(sqr)
+        sqrt = get_sqrt(poly)
+        drv = get_inv(sqrt)
+        res = get_prd(drv, op.diff)
+      when "atanh"
+        op = @operands.first
+        sqr = get_sqr(op)
+        diff = get_x_m_one(sqr, true)
+        drv = get_inv(diff)
+        res = get_prd(drv, op.diff)
+      when "exp"
+        op = @operands.first
+        drv = Func.new("exp", 1, nil, 0, [op])
+        res = get_prd(drv, op.diff)
+      when "log"
+        op = @operands.first
+        drv = get_inv(op)
+        res = get_prd(drv, op.diff)
+      when "sqrt"
+        op = @operands.first
+        sqrt = get_sqrt(op)
+        drv = get_inv(sqrt, false, "0.5")
+        res = get_prd(drv, op.diff)
+      when "pow"
+        base = @operands[0]
+        pow = @operands[1]
+        log = Func.new("log", 1, nil, 0, [base])
+        to_exp_ops = get_prd(log, pow)
+        to_exp = Func.new("exp", 1, nil, 0, [to_exp_ops])
+        res = to_exp.diff
+      else
+        fail "Unknown functions"
+      end
+      res
+    end
+
+    def to_s
+      ops = @operands.map{ |o| o.to_s }
+      infix = /^[a-z]/.match(@fn).nil?
+      if infix
+        if ExprTree.cmp_fn.map{ |c| c.first }.include?(@fn)
+          ops = ops.map{ |o| "std::abs(#{o})" }
+        end
+        res = "(" + ops.join(" #{@fn} ") + ")"
+      else
+        res = @fn + "(" + ops.join(", ") + ")"
+      end
+      res
+    end
+  end # class Func
+
+  class Ternary < Node
+    def initialize(level)
+      super(level)
+      cond = generate_operand(:log_fn, level + 1)
+      op1 = generate_operand(:simple_fn, level + 1)
+      op2 = generate_operand(:simple_fn, level + 1)
+      @operands = [cond, op1, op2]
+    end
+
+    def to_s
+      ops = @operands.map{ |o| o.to_s }
+      "(" + ops[0] + ") ? (" + ops[1] + ") : (" + ops[2] + ")"
+    end
+  end # class Ternary
+
+  class Leaf < Node
+    def initialize(fn)
+      case fn
+      when Point
+        @fn = fn
+      when Num
+        @fn = "ValType(#{$rng.rand()}, #{$rng.rand()})"
+      else
+        @fn = fn
+      end
+    end
+
+    def diff
+      case @fn
+      when Point
+        r = Leaf.new('1.0')
+      else
+        r = Leaf.new('0.0')
+      end
+      r
+    end
+
+    def to_s
+      @fn
+    end
+  end # class Leaf
+
+  def self.simple_fn
+    if ExprTree.instance_variable_defined?(:@fns)
+      ExprTree.instance_variable_get(:@fns)
+    else
+      ExprTree.instance_variable_set(:@fns, [["+", nil, :simple_fn],
+                                             ["-", nil, :simple_fn],
+                                             ["*", nil, :simple_fn],
+                                             ["/", nil, :simple_fn],
+                                             ["sin", 1, :simple_fn],
+                                             ["cos", 1, :simple_fn],
+                                             ["tan", 1, :simple_fn],
+                                             ["asin", 1, :simple_fn],
+                                             ["acos", 1, :simple_fn],
+                                             ["atan", 1, :simple_fn],
+                                             ["sinh", 1, :simple_fn],
+                                             ["cosh", 1, :simple_fn],
+                                             ["tanh", 1, :simple_fn],
+                                             ["asinh", 1, :simple_fn],
+                                             ["acosh", 1, :simple_fn],
+                                             ["atanh", 1, :simple_fn],
+                                             ["exp", 1, :simple_fn],
+                                             ["log", 1, :simple_fn],
+                                             ["sqrt", 1, :simple_fn],
+                                             ["pow", 2, :simple_fn]])
+    end
+  end
+
+  def self.cmp_fn
+    if ExprTree.instance_variable_defined?(:@cmps)
+      ExprTree.instance_variable_get(:@cmps)
+    else
+      ExprTree.instance_variable_set(:@cmps, [["<", 2, :simple_fn],
+                                              [">", 2, :simple_fn],
+                                              ["<=", 2, :simple_fn],
+                                              [">=", 2, :simple_fn],
+                                              ["==", 2, :simple_fn],
+                                              ["!=", 2, :simple_fn]])
+    end
+  end
+
+  def self.log_fn
+    if ExprTree.instance_variable_defined?(:@logs)
+      ExprTree.instance_variable_get(:@logs)
+    else
+      ExprTree.instance_variable_set(:@logs, [["&&", nil, :cmp_fn],
+                                              ["||", nil, :cmp_fn]])
+    end
+  end
+
+  class Expr
+    def initialize(node = nil)
+      if node
+        @fn = node
+      else
+        if $generate_ternary
+          @fn = Ternary.new(1)
+        else
+          sfn = ExprTree.simple_fn
+          sz = sfn.size
+          sz = sz < 4 ? sz : 4
+          ops = sfn[$rng.rand(sz)]
+          @fn = Func.new(*ops, 1)
+        end
+      end
+    end
+
+    def to_s
+      if @expr.nil?
+        @expr = @fn.to_s
+      else
+        @expr
+      end
+    end
+
+    def diff
+      Expr.new(@fn.diff)
+    end
+  end # class Expr
+
+end # module ExprTree
--- frac-gen.rb
+++ frac-gen.rb
@@ -6,6 +6,8 @@ require 'pp'
 require 'erb'
 require 'fileutils'
 
+require_relative 'Scripts/exprtree'
+
 options = {}
 OptionParser.new do |opts|
   opts.on("-s", "--seed NUM", "Generate with initial seed") { |v| options[:seed] = v }
@@ -16,9 +18,6 @@ OptionParser.new do |opts|
   opts.on("-a", "--with-abs VAL", "Find absolute value of function, not zeros") { |v| options[:abs] = v }
   opts.on("-d", "--diff-expr EXPR", "Considered to be first derivative of expression specified in --expr") { |v| options[:diff] = v }
 end.parse!
-
-notern = options[:notern] == true
-$abs = options[:abs]
 
 $params = []
 $need_diff = false
@@ -56,6 +55,14 @@ else
   fail "Unknown method"
 end
 
+notern = options[:notern] == true
+ExprTree.set_tern_mode(!notern)
+
+seed = (options[:seed] || Time.now).to_i
+ExprTree.init_rng(seed)
+
+$abs = options[:abs]
+
 # Check generator mode paramenters.
 if options[:expr].nil?
   if $need_diff and (!$abs.nil? or notern != true)
@@ -64,414 +71,6 @@ if options[:expr].nil?
 else
   if $need_diff and options[:diff].nil?
     fail "Method needs derivative of specified function!"
-  end
-end
-
-$seed = (options[:seed] || Time.now).to_i
-$rng = Random.new($seed)
-
-NM = "std::"
-
-class Func
-  attr_reader :fn
-  attr_reader :optype
-  attr_accessor :operands
-
-  def initialize(fn, arity, optype, operands = nil)
-    @fn = /^[[:alpha]]/.match(fn) ? NM + fn : fn
-    @arity = arity
-    @optype = optype
-    @operands = operands if operands
-  end
-
-  def arity
-    @arity ? @arity : $rng.rand(2..3)
-  end
-
-  def n(func)
-    Node.new(func)
-  end
-
-  def get_inv(op, neg = false, value = nil)
-    if value
-      val = value
-    else
-      val = neg ? "-1.0" : "1.0"
-    end
-    n(Func.new("/", 2, nil, [n(Leaf.new(val)), op]))
-  end
-
-  def get_sqr(op)
-    n(Func.new("*", 2, nil, [op, op]))
-  end
-
-  def get_prd(op1, op2)
-    n(Func.new("*", 2, nil, [op1, op2]))
-  end
-
-  def get_x_m_one(op, inv = false)
-    ops = [op, n(Leaf.new("1.0"))]
-    ops = ops.reverse if inv
-    n(Func.new("-", 2, nil, ops))
-  end
-
-  def get_x_p_one(op, inv = false)
-    n(Func.new("+", 2, nil, [op, n(Leaf.new("1.0"))]))
-  end
-
-  def get_sqrt(op)
-    n(Func.new("sqrt", 1, nil, [op]))
-  end
-
-  def diff
-    func = @fn.sub(NM, "")
-    case func
-    when "+", "-"
-      new_ops = @operands.map{ |o| o.diff }
-      res = Func.new(func, new_ops.size, nil, new_ops)
-    when "*"
-      new_ops = Array.new
-      @operands.each_with_index do |o, i|
-        ops = @operands[0...i] + @operands[(i + 1)..-1]
-        mult = n(Func.new("*", @operands.size, nil, [o.diff] + ops))
-        new_ops << mult
-      end
-      res = Func.new("+", new_ops.size, nil, new_ops)
-    when "/"
-      # Like * but should invert all but first operands.
-      if @operands.size != 2
-        invops = Array.new
-        invops = @operands[1..-1].map do |op|
-          get_inv(op)
-        end
-        invops << @operands[0]
-        inverted = Func.new("*", @operands.size, nil, invops)
-        res = inverted.diff
-      else
-        op1 = @operands[0]
-        op2 = @operands[1]
-        sqr = get_sqr(op2)
-        op1d = op1.diff
-        op2d = op2.diff
-        lhs = get_prd(op1d, op2)
-        rhs = get_prd(op1, op2d)
-        diff = n(Func.new("-", 2, nil, [lhs, rhs]))
-        res = Func.new("/", 2, nil, [diff, sqr])
-      end
-    when "sin"
-      op = @operands.first
-      drv = n(Func.new("cos", 1, nil, [op]))
-      res = get_prd(drv, op.diff)
-    when "cos"
-      op = @operands.first
-      dcos = n(Func.new("sin", 1, nil, [op]))
-      drv = n(Func.new("*", 2, nil, [dcos, n(Leaf.new("-1.0"))]))
-      res = get_prd(drv, op.diff)
-    when "tan"
-      op = @operands.first
-      cos = n(Func.new("cos", 1, nil, [op]))
-      sqr = get_sqr(cos)
-      drv = get_inv(sqr)
-      res = get_prd(drv, op.diff)
-    when "asin", "acos"
-      op = @operands.first
-      sqr = get_sqr(op)
-      diff = get_x_m_one(sqr, true)
-      sqrt = get_sqrt(diff)
-      drv = get_inv(sqrt, func == "acos")
-      res = get_prd(drv, op.diff)
-    when "atan"
-      op = @operands.first
-      sqr = get_sqr(op)
-      sum = get_x_p_one(sqr)
-      drv = get_inv(sum)
-      res = get_prd(drv, op.diff)
-    when "sinh"
-      op = @operands.first
-      drv = n(Func.new("cosh", 1, nil, [op]))
-      res = get_prd(drv, op.diff)
-    when "cosh"
-      op = @operands.first
-      drv = n(Func.new("sinh", 1, nil, [op]))
-      res = get_prd(drv, op.diff)
-    when "tanh"
-      op = @operands.first
-      cosh = n(Func.new("cosh", 1, nil, [op]))
-      sqr = get_sqr(cosh)
-      drv = get_inv(sqr)
-      res = get_prd(drv, op.diff)
-    when "asinh", "acosh"
-      op = @operands.first
-      sqr = get_sqr(op)
-      poly = func == "asinh" ? get_x_p_one(sqr) : get_x_m_one(sqr)
-      sqrt = get_sqrt(poly)
-      drv = get_inv(sqrt)
-      res = get_prd(drv, op.diff)
-    when "atanh"
-      op = @operands.first
-      sqr = get_sqr(op)
-      diff = get_x_m_one(sqr, true)
-      drv = get_inv(diff)
-      res = get_prd(drv, op.diff)
-    when "exp"
-      op = @operands.first
-      drv = n(Func.new("exp", 1, nil, [op]))
-      res = get_prd(drv, op.diff)
-    when "log"
-      op = @operands.first
-      drv = get_inv(op)
-      res = get_prd(drv, op.diff)
-    when "sqrt"
-      op = @operands.first
-      sqrt = get_sqrt(op)
-      drv = get_inv(sqrt, false, "0.5")
-      res = get_prd(drv, op.diff)
-    when "pow"
-      base = @operands[0]
-      pow = @operands[1]
-      log = n(Func.new("log", 1, nil, [base]))
-      to_exp_ops = get_prd(log, pow)
-      to_exp = n(Func.new("exp", 1, nil, [to_exp_ops]))
-      res = to_exp.diff
-    else
-      fail "Unknown functions"
-    end
-    res.is_a?(Node) ? res : n(res)
-  end
-
-end
-
-class Ternary
-  def initialize
-  end
-
-  def operands
-    if @operands
-      @operands
-    else
-      cond = Node.new(log_fn[$rng.rand(log_fn.size)].clone)
-      op1 = Node.new(simple_fn[$rng.rand(simple_fn.size)].clone)
-      op2 = Node.new(simple_fn[$rng.rand(simple_fn.size)].clone)
-      @operands = [cond, op1, op2]
-    end
-  end
-end
-
-Point = 'Pt'
-Num = 'num'
-
-class Leaf
-  attr_reader :fn
-
-  def initialize(fn)
-    case fn
-    when Point
-      @fn = fn
-    when Num
-      @fn = "ValType(#{$rng.rand()}, #{$rng.rand()})"
-    else
-      @fn = fn
-    end
-  end
-
-  def arity
-    0
-  end
-
-  def operands
-    []
-  end
-
-  def diff
-    case @fn
-    when Point
-      r = Leaf.new('1.0')
-    else
-      r = Leaf.new('0.0')
-    end
-    Node.new(r)
-  end
-end
-
-$main = self
-
-def simple_fn
-  if $main.instance_variable_defined?(:@fns)
-    $main.instance_variable_get(:@fns)
-  else
-    $main.instance_variable_set(:@fns, [
-                                  ["+", nil, :simple_fn],
-                                  ["-", nil, :simple_fn],
-                                  ["*", nil, :simple_fn],
-                                  ["/", nil, :simple_fn],
-                                  ["sin", 1, :simple_fn],
-                                  ["cos", 1, :simple_fn],
-                                  ["tan", 1, :simple_fn],
-                                  ["asin", 1, :simple_fn],
-                                  ["acos", 1, :simple_fn],
-                                  ["atan", 1, :simple_fn],
-                                  ["sinh", 1, :simple_fn],
-                                  ["cosh", 1, :simple_fn],
-                                  ["tanh", 1, :simple_fn],
-                                  ["asinh", 1, :simple_fn],
-                                  ["acosh", 1, :simple_fn],
-                                  ["atanh", 1, :simple_fn],
-                                  ["exp", 1, :simple_fn],
-                                  ["log", 1, :simple_fn],
-                                  ["sqrt", 1, :simple_fn],
-                                  ["pow", 2, :simple_fn]
-                                ].map{ |args| Func.new(*args) })
-  end
-end
-
-def cmp_fn
-  if $main.instance_variable_defined?(:@cmps)
-    $main.instance_variable_get(:@cmps)
-  else
-    $main.instance_variable_set(:@cmps, [["<", 2, :simple_fn],
-                                         [">", 2, :simple_fn],
-                                         ["<=", 2, :simple_fn],
-                                         [">=", 2, :simple_fn],
-                                         ["==", 2, :simple_fn],
-                                         ["!=", 2, :simple_fn],
-                                        ].map{ |args| Func.new(*args) })
-  end
-end
-
-def log_fn
-  if $main.instance_variable_defined?(:@logs)
-    $main.instance_variable_get(:@logs)
-  else
-    $main.instance_variable_set(:@logs, [["&&", nil, :cmp_fn],
-                                         ["||", nil, :cmp_fn],
-                                        ].map{ |args| Func.new(*args) })
-  end
-end
-
-class BadExpr < StandardError
-end
-
-if notern
-  $tern_prob = 1.0
-  sz = simple_fn.size
-  sz = sz < 4 ? sz : 4
-  $init_func = lambda { simple_fn[$rng.rand(sz)].clone }
-  $max_prob = 2.0
-else
-  $tern_prob = 1.2
-  $init_func = lambda { Ternary.new }
-  $max_prob = 2.5
-end
-
-$level = nil
-
-class Node
-  attr_reader :operands
-  attr_reader :fn
-
-  def initialize(fn)
-    if $level == 2000
-      $level = 0
-      fail BadExpr
-    end
-    $level += 1
-
-    @fn = fn
-    # If function has predefined set of operands
-    # (like ternary operator) just use it.
-    # Otherwise generate some new operands.
-    if @fn.operands
-      @operands = @fn.operands
-    else
-      @operands = Array.new(fn.arity).map do |op|
-        if (@fn.optype != :simple_fn)
-          sel = $main.send(@fn.optype)
-          fn = sel[$rng.rand(sel.size)]
-          Node.new(fn.clone)
-        else
-          r = $rng.rand(simple_fn.size * $max_prob)
-          # Simple function.
-          if (r < simple_fn.size)
-            fn = simple_fn[$rng.rand(simple_fn.size)]
-            Node.new(fn.clone)
-          # Ternary.
-          elsif r < simple_fn.size * $tern_prob
-            Node.new(Ternary.new)
-          # Leaf.
-          else
-            Node.new(Leaf.new($rng.rand() < 0.7 ? Point : Num))
-          end
-        end
-      end
-      @fn.operands = @operands
-    end
-  end
-
-  def to_s
-    ops = @operands.map{ |o| o.to_s }
-    res = case
-          when @fn.is_a?(Ternary)
-            ops[0].to_s + " ? " + ops[1].to_s + " : "  + ops[2].to_s
-          when ['*','/','+','-','**'].include?(@fn.fn)
-            ops.join(" #{@fn.fn} ")
-          when log_fn.map{ |f| f.fn }.include?(@fn.fn)
-            ops.map{ |o| o.to_s }.join(" #{@fn.fn} ")
-          when cmp_fn.map{ |f| f.fn }.include?(@fn.fn)
-            ops.map{ |o| "std::abs(#{o.to_s})" }.join(" #{@fn.fn} ")
-          when @fn.is_a?(Leaf)
-            @fn.fn
-          else
-            @fn.fn + "(" + ops.join(", ") + ")"
-          end
-    "(" + res + ")"
-  end
-
-  def diff
-    unless simple_fn.any?{ |f| f.fn == @fn.fn } || @fn.is_a?(Leaf)
-      fail "Can get derivative only from simple functions"
-    end
-    @fn.diff
-  end
-
-  def evaluate
-    pt = 12.0
-    expr = to_s.gsub('num') { $rng.rand().to_s }
-    eval(expr, binding)
-  end
-end
-
-class Expr
-  def initialize(node = nil)
-    $level = 0
-    if node
-      @fn = node
-    else
-      @fn = Node.new($init_func.call)
-    end
-  end
-
-  def to_s
-    if @expr.nil?
-      if $abs
-        fn = "std::abs(#{@fn}) - ValType(#{$abs}, 0.0)"
-      else
-        fn = "#{@fn}"
-      end
-      @expr = "ValType Fn1 = #{fn};\n"
-      @expr += "return Fn1;"
-    else
-      @expr
-    end
-  end
-
-  def diff
-    $level = 0
-    Expr.new(@fn.diff)
-  end
-
-  def evaluate(x)
-    pt = x
-    eval(to_s, binding)
   end
 end
 
@@ -484,16 +83,16 @@ end
 
 FRACMATH = 'FracMath.cpp'
 
-$num = $seed
+$num = seed
 Dir.mkdir("Images") unless Dir.exist?("Images")
 if options[:dir]
   $dir = options[:dir]
 else
-  $dir = File.join("Images", "#{$seed.to_s}_#{$method}")
+  $dir = File.join("Images", "#{seed.to_s}_#{$method}")
   Dir.mkdir($dir)
 end
 $log = File.open(File.join($dir, "last_seed.txt"), "w")
-$log << "Seed: #{$seed}\n"
+$log << "Seed: #{seed}\n"
 
 def generate_image(expr, expr_diff)
   method = $method
@@ -517,23 +116,33 @@ def generate_image(expr, expr_diff)
   FileUtils.mv(fi, $dir)
 end
 
-DEFAULT_DIFF = "abort(); return 0.0;"
+DEFAULT_EXPR = "abort(); return 0.0;"
+
+def wrap_expr(expr)
+  if expr.nil?
+    DEFAULT_EXPR
+  else
+    expr = "std::abs(#{expr}) - #{$abs}" if $abs
+    "return #{expr};"
+  end
+end
 
 if options[:expr].nil?
   loop do
     break if $stop
     begin
-      expr_tree = Expr.new
+      expr_tree = ExprTree::Expr.new
       expr_tree_diff = expr_tree.diff if $need_diff
-    rescue BadExpr => e
+    rescue ExprTree::BadExpr => e
       next
     end
-    expr = expr_tree.to_s
+    expr = wrap_expr(expr_tree.to_s)
     if $need_diff
       expr_diff = expr_tree_diff.to_s
     else
-      expr_diff = DEFAULT_DIFF
+      expr_diff = nil
     end
+    expr_diff = wrap_expr(expr_diff)
     puts expr
     puts expr_diff
     generate_image(expr, expr_diff)
@@ -541,7 +150,7 @@ if options[:expr].nil?
   end
 else
   expr = options[:expr]
-  expr_diff = options[:diff] || DEFAULT_DIFF
+  expr_diff = options[:diff] || DEFAULT_EXPR
   subst_num = 0.0
   loop do
     expr_subst = expr.sub("NUM", subst_num.to_s)
